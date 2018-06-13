d1014d710711d3ed9e9b6680f03990f0d0a8bea7
Carolyn Yates <chyatesil@gmail.com>
2018-06-13 16:42:20 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FaqComponent } from './faq/faq.component';
+import { SubmitComponent } from './submit/submit.component';
+import { SurveyComponent } from './survey/survey.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  // { path: '', pathMatch: 'full', component: HomeComponent  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-
+import { FaqComponent } from './faq/faq.component';
+import { SubmitComponent } from './submit/submit.component';
+import { SurveyComponent } from './survey/survey.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    FaqComponent,
+    SubmitComponent,
+    SurveyComponent,
+    ContactComponent,
+    HomeComponent,
+    AboutComponent
   ],
+
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
